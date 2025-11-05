cd24a577c6570e3d8ddb5cfc3097a2af90a00966
Dimetrius-eng <mydimka08@gmail.com>
2025-11-05 13:23:11 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-// ВЕРСІЯ 19 - Виправлення логіки кнопки "Вийти в меню"
-const CACHE_NAME = 'it-alias-v19-quit-logic-fix';
+// ВЕРСІЯ 20 - Додано звуки
+const CACHE_NAME = 'it-alias-v20-sounds';
 
+// ЗМІНА ТУТ: Додано звуки у список
 const urlsToCache = [
   './',
   './index.html',
@@ -10,7 +11,12 @@ const urlsToCache = [
   './words.json',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
-  'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap'
+  'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap',
+  // Нова папка
+  './sounds/correct.mp3',
+  './sounds/skip.mp3',
+  './sounds/times-up.mp3',
+  './sounds/tick.mp3'
 ];
 
 // 1. Подія "install"
@@ -18,7 +24,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Відкрито кеш v19');
+        console.log('Відкрито кеш v20');
         const localUrls = urlsToCache.filter(url => !url.startsWith('http'));
         const externalUrls = urlsToCache.filter(url => url.startsWith('http'));
         
@@ -28,11 +34,11 @@ self.addEventListener('install', event => {
             return Promise.all(externalRequests.map(req => cache.add(req)));
           });
       })
-      .catch(err => console.error('Помилка cache.addAll у v19:', err))
+      .catch(err => console.error('Помилка cache.addAll у v20 (Можливо, відсутні звуки?):', err))
   );
 });
 
-// 2. Подія "fetch"
+// 2. Подія "fetch" (без змін)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
@@ -42,9 +48,9 @@ self.addEventListener('fetch', event => {
   );
 });
 
-// 3. Подія "activate"
+// 3. Подія "activate" (оновлюємо "білий список")
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME]; 
+  const cacheWhitelist = [CACHE_NAME]; // Залишити тільки v20
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
@@ -57,7 +63,7 @@ self.addEventListener('activate', event => {
       );
     })
     .then(() => {
-        console.log('Service Worker v19 активовано і перехоплює контроль!');
+        console.log('Service Worker v20 активовано і перехоплює контроль!');
         return self.clients.claim();
     })
   );
